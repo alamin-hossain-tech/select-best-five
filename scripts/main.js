929408fcb60ee67ba865f95a2cf019ba65602655
Al-Amin Hossain <alaminnishat@gmail.com>
2022-08-22 21:03:57 +0600

    all player unique id added

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,20 @@
 const ol = document.getElementById("ol");
 const li = document.createElement("li");
-
+const playerList = document.querySelectorAll("li");
 // Function For selecting player
 function selectPlayer(player, playerBtn) {
   ol.appendChild(li);
   li.innerText = document.getElementById(player).innerText;
   document.getElementById(playerBtn).setAttribute("disabled", true);
 }
+if (playerList.length <= 5) {
+  document.getElementById("messi-btn").addEventListener("click", function () {
+    selectPlayer("messi", "messi-btn");
+  });
+}
 
-document.getElementById("messi-btn").addEventListener("click", function () {
-  selectPlayer("messi", "messi-btn");
-});
 // document.getElementById("messi-btn").addEventListener("click", function () {
 //   li.innerText = "Lionel Messi";
 //   ol.appendChild(li);
 //   document.getElementById("messi-btn").setAttribute("disabled", true);
 // });
-
-// const olNode = document.querySelectorAll("li");
-// console.log(olNode);
